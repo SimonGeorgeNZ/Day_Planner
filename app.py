557108c3f1563901516e85b5473818dc629c9a63
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2022-10-23 10:39:18 +0000

    added app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+# Imports and configurations #
+
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "Milestone_three"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
