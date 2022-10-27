b9fa39a4bc8aea0999adc9713a59882db5946546
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2022-10-27 01:12:39 +0000

    Working as normal, html file not in templates folder

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-# Imports and configurations #
+# # Imports and configurations #
 
-import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from os import path
-if path.exists("env.py"):
-    import env
+# import os
+# from flask import Flask, render_template, redirect, request, url_for
+# from flask_pymongo import PyMongo
+# from bson.objectid import ObjectId
+# from os import path
+# if path.exists("env.py"):
+#     import env
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = "Milestone_three"
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+# app.config["MONGO_DBNAME"] = "Milestone_three"
+# app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
-mongo = PyMongo(app)
-
+# mongo = PyMongo(app)
