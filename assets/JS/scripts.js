4e9df82b8bb59db1140fc15cfc1643573be5e500
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2022-11-04 04:59:14 +0000

    Change buttons functions linking properly

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,12 +1,18 @@
 function changeBlue () {
     document.getElementById('bgColor').style.backgroundColor = "Blue";
+    document.getElementById('inner').innerHTML = "Test 1";
+    document.getElementById('bgColor').style.display = "block";
 }
 
 function changeRed () {
     document.getElementById('bgColor').style.backgroundColor = "Red";
+    document.getElementById('inner').innerHTML = "Test 2";
+    document.getElementById('bgColor').style.display = "block";
 }
 
 function changeWhite () {
     document.getElementById('bgColor').style.backgroundColor = "White";
+    document.getElementById('bgColor').style.display = "none";
 }
+
 
