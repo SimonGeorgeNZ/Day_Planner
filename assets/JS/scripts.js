1491f3964ec9020ceea7e5480b72a997e8d9657b
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2022-11-04 08:15:53 +0000

    Calendar JS file linked and working

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,18 +1,27 @@
-function changeBlue () {
-    document.getElementById('bgColor').style.backgroundColor = "Blue";
-    document.getElementById('inner').innerHTML = "Test 1";
-    document.getElementById('bgColor').style.display = "block";
+$(document).ready(function () {
+    document.getElementById('innerMonth').style.display = "block";
+    document.getElementById('innerDay').style.display = "none";
+    document.getElementById('innerWeek').style.display = "none";
+});
+
+function changeMonth () {
+    document.getElementById('innerMonth').style.display = "block";
+    document.getElementById('innerWeek').style.display = "none";
+    document.getElementById('innerDay').style.display = "none";
 }
 
-function changeRed () {
-    document.getElementById('bgColor').style.backgroundColor = "Red";
-    document.getElementById('inner').innerHTML = "Test 2";
-    document.getElementById('bgColor').style.display = "block";
+function changeWeek () {
+    document.getElementById('innerWeek').style.display = "block";
+    document.getElementById('innerDay').style.display = "none";
+    document.getElementById('innerMonth').style.display = "none";
 }
 
-function changeWhite () {
-    document.getElementById('bgColor').style.backgroundColor = "White";
-    document.getElementById('bgColor').style.display = "none";
+function changeDay () {
+    document.getElementById('innerDay').style.display = "block";
+    document.getElementById('innerWeek').style.display = "none";
+    document.getElementById('innerMonth').style.display = "none";
 }
+
+
 
 
