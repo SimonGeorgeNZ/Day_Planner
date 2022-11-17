0dbf1e2ca5465e6415fc4b1d2a8b41d82962013a
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2022-11-17 02:13:59 +0000

    february returning correct amount of days

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,3 +1,29 @@
+// var year = new Date().getFullYear();
+var year = 2024;
+let january = 31;
+if (year % 4 == 0){
+    var february = 29
+} else {
+    var february = 28
+}
+let march = 31;
+let april = 30;
+let may = 31;
+let june = 30;
+let july = 31;
+let august = 31;
+let september = 30;
+let october = 31;
+let november = 30;
+let december = 31;
+
+// if (year % 4 == 0){
+//     console.log("yes");}
+
+console.log(year);
+
+console.log(february);
+
 $(document).on('DOMContentLoaded', function () {
     document.getElementById('innerMonth').style.display = "d-block";
     document.getElementById('innerDay').style.display = "none";
@@ -21,6 +47,14 @@ function changeDay () {
     document.getElementById('innerWeek').style.display = "none";
     document.getElementById('innerMonth').style.display = "none";
 }
+
+// function createMonth () {
+//     document.getElementById('innerMonth');
+//     const dayBox = document.createElement('div');
+//     dayBox.className = 'col-sm-4 displayMonth bb layout';
+//     dayBox.innerHTML = 'Month';
+//     document.getElementById('innerMonth').appendChild(dayBox);
+// }
 
 
 
